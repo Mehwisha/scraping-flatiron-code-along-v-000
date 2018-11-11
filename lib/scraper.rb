0a09f8505e6e73662b95ec81ba5bc8a5e820d6cd
Mehwish Aziz <mehwisha@msn.com>
2018-11-11 03:09:27 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,9 +5,9 @@ require_relative './course.rb'
 
 class Scraper
   def get_page
-
+doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
   end
-  
+
     def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -18,9 +18,6 @@ class Scraper
       end
     end
   end
-  
-  
+
+
 end
-
-
-
