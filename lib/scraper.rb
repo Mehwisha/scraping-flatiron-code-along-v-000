dc801aec5a3e6ca6533d4a724dfaf680b37ad69b
Mehwish Aziz <mehwisha@msn.com>
2018-11-11 03:16:17 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -15,6 +15,9 @@ doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scrapin
 #       course.description = post.css("p").text
 #   end
 end
+def get_courses
+  self.get_page.css(".post")
+end
 
     def print_courses
     self.make_courses
